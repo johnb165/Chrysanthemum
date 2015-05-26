aded2b499c6979ba9cbb2a3ab13e86b6f4b50d22
FluffyPancakes <fluffypankakes@icloud.com>
2015-05-25 20:27:26 -0400

    More Works

--- src/me/FluffyPancakes/Chrysanthemum/Main.java
+++ src/me/FluffyPancakes/Chrysanthemum/Main.java
@@ -1,5 +1,47 @@
 package me.FluffyPancakes.Chrysanthemum;
 
-public class Main {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.java.JavaPlugin;
 
+public class Main extends JavaPlugin implements Listener {
+
+	public static Main plugin;
+	
+	@Override
+	public void onEnable() {
+		plugin = this;
+		PluginManager manager = getServer().getPluginManager();
+		manager.registerEvents(this, this);
+		getLogger().info("- Enabled");
+	}
+	
+	@Override
+	public void onDisable() {
+		getLogger().info("- Disabled");
+		plugin = null;
+	}
+	
+	public static Plugin getPlugin() {
+		return plugin;
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] a) {
+		Player player = (Player)sender;		
+		if (cmd.getName().equalsIgnoreCase("chrysanthemum")) {
+			if (a.length == 0) {
+				player.sendMessage("&d&m------------".replaceAll("&", "§") + "&5&l[&d&lChrysanthemum&5&l]" + "&c&m------------".replaceAll("&", "§"));
+				player.sendMessage("   &d".replaceAll("&", "§"));
+				player.sendMessage("   &d".replaceAll("&", "§"));
+				player.sendMessage("   &d".replaceAll("&", "§"));
+				player.sendMessage("&d&m-----------------------------------------".replaceAll("&", "§"));
+				return true;
+			}
+		}
+		return false;	
+	}
 }
