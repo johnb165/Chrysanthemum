e84575507acd5ccf6dd2c482bea5dd5ae2ad41e8
FluffyPancakes <fluffypankakes@icloud.com>
2015-05-26 07:42:47 -0400

    Fixes
    *Added Vault
    *Fixed Packages

--- src/me/FluffyPancakes/Chrysanthemum/Main.java
+++ src/me/FluffyPancakes/Chrysanthemum/Main.java
@@ -2,7 +2,8 @@ package me.FluffyPancakes.Chrysanthemum;
 
 import java.io.IOException;
 
-import metrics.src.main.java.org.mcstats.Metrics;
+import org.mcstats.Metrics;
+import net.milkbowl.vault.economy.Economy;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
@@ -10,11 +11,13 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Main extends JavaPlugin implements Listener {
 
 	public static Main plugin;
+    public static Economy econ = null;
 	
 	@Override
 	public void onEnable() {
@@ -30,6 +33,11 @@ public class Main extends JavaPlugin implements Listener {
 			getLogger().info("Failed To Connect To Metrics D:");
 			
 		}
+		if (!setupEconomy() ) {
+            getLogger().info(String.format("[%s] Disabled Due To No Vault Dependency Found!", getDescription().getName()));
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
 	}
 	
 	@Override
@@ -46,11 +54,11 @@ public class Main extends JavaPlugin implements Listener {
 		Player player = (Player)sender;		
 		if (cmd.getName().equalsIgnoreCase("chrysanthemum")) {
 			if (a.length == 0) {
-				player.sendMessage("&d&m------------&5&l[&d&lChrysanthemum&5&l]&c&m------------".replaceAll("&", "§"));
+				player.sendMessage("&d&m------------&5&l[&d&lChrysanthemum&5&l]&d&m------------".replaceAll("&", "§"));
 				player.sendMessage("   &d".replaceAll("&", "§"));
 				player.sendMessage("   &d".replaceAll("&", "§"));
 				player.sendMessage("   &d".replaceAll("&", "§"));
-				player.sendMessage("&d&m-------------------------------------------".replaceAll("&", "§"));
+				player.sendMessage("&d&m---------------------------------------".replaceAll("&", "§"));
 				return true;
 			}
 		}
@@ -59,4 +67,17 @@ public class Main extends JavaPlugin implements Listener {
 		}
 		return false;	
 	}
+	
+
+    private boolean setupEconomy() {
+        if (getServer().getPluginManager().getPlugin("Vault") == null) {
+            return false;
+        }
+        RegisteredServiceProvider<Economy> rsp = getServer().getServicesManager().getRegistration(Economy.class);
+        if (rsp == null) {
+            return false;
+        }
+        econ = rsp.getProvider();
+        return econ != null;
+    }
 }
